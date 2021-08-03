14a8f6b4ac8a8c04a4dfcf119be0f183e37139d0
Oliver Kaufmann <okaufmann91@gmail.com>
2021-08-03 16:32:45 +0200

    wip

--- build.sh
+++ build.sh
@@ -12,6 +12,4 @@ echo "replacing version"
 sed "s/{VERSION}/$newVersion/g" info.plist.example > info.plist
 
 echo "create zip"
-zip -r "../$fileName" . -x "*.DS_Store" -x "*/\.DS_Store" -x "info.plist.example"
-
-open $fileName
+zip -r "../$fileName" . -x "*.DS_Store" -x "*/\.DS_Store" -x "info.plist.example" -x ".git"
